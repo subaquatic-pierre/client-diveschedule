0a84b7e478b75ce4e1570582d36aa2f54f219e52
subaquatic-pierre <subaquatic.pierre@gmail.com>
2021-05-11 15:19:59 +0400

    Updated alert styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { CssBaseline, ThemeProvider, Container } from "@material-ui/core";
+import {
+  CssBaseline,
+  ThemeProvider,
+  Container,
+  makeStyles,
+} from "@material-ui/core";
 import { AlertContext, initialAlert } from "../../App";
 import Alert from "@material-ui/lab/Alert";
 
@@ -7,7 +12,22 @@ import { Header } from "../Header/Header";
 import { Footer } from "../Footer";
 import theme from "../../theme";
 
+const useStyles = makeStyles((theme) => ({
+  alertContainer: {
+    position: "absolute",
+    zIndex: theme.zIndex.modal,
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  alert: {
+    position: "relative",
+    boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px;",
+  },
+}));
+
 export const Layout: React.FC = ({ children }) => {
+  const classes = useStyles();
   const {
     alert: { state, severity, message },
     setAlert,
@@ -18,9 +38,15 @@ export const Layout: React.FC = ({ children }) => {
         <CssBaseline />
         <Header />
         {state && (
-          <Alert onClose={() => setAlert(initialAlert)} severity={severity}>
-            {message}
-          </Alert>
+          <div className={classes.alertContainer}>
+            <Alert
+              className={classes.alert}
+              onClose={() => setAlert(initialAlert)}
+              severity={severity}
+            >
+              {message}
+            </Alert>
+          </div>
         )}
         <Container maxWidth="xl">{children}</Container>
         <Footer />
